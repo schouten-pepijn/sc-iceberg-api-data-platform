08ccba06a4600d2d359129b19fe614cf0991e7cd
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-21 22:41:52 +0100

    feat: add gold weather fact table (weather)

--- orchestration/assets/weather.py
+++ orchestration/assets/weather.py
@@ -1,6 +1,7 @@
 import dagster as dg
 
 from pipelines.ingestion.write_bronze_weather import run as write_bronze_weather
+from pipelines.transformations.write_fact_weather import run as write_fact_weather
 from pipelines.transformations.write_silver_weather import run as write_silver_weather
 
 
@@ -14,3 +15,9 @@ def bronze_weather(context: dg.AssetExecutionContext) -> None:
 def silver_weather_hourly(context: dg.AssetExecutionContext) -> None:
     context.log.info("Transforming weather data into silver layer...")
     write_silver_weather()
+
+
+@dg.asset(deps=[silver_weather_hourly])
+def fact_weather(context: dg.AssetExecutionContext) -> None:
+    context.log.info("Aggregating weather data into gold layer...")
+    write_fact_weather()
--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,5 +1,9 @@
 from dagster import Definitions
 
-from orchestration.assets.weather import bronze_weather, silver_weather_hourly
+from orchestration.assets.weather import (
+    bronze_weather,
+    fact_weather,
+    silver_weather_hourly,
+)
 
-defs = Definitions(assets=[bronze_weather, silver_weather_hourly])
+defs = Definitions(assets=[bronze_weather, silver_weather_hourly, fact_weather])

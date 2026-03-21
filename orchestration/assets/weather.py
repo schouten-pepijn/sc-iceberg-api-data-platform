3c3ab09ccd03aa5f66d7b287285ab04e53706f8e
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-22 00:24:52 +0100

    feat: add dim_location to dagster (orchestration)

--- orchestration/assets/weather.py
+++ orchestration/assets/weather.py
@@ -1,11 +1,18 @@
 import dagster as dg
 
+from pipelines.ingestion.ingest_locations import run as ingest_locations
 from pipelines.ingestion.write_bronze_weather import run as write_bronze_weather
 from pipelines.transformations.write_fact_weather import run as write_fact_weather
 from pipelines.transformations.write_silver_weather import run as write_silver_weather
 
 
 @dg.asset
+def dim_location(context: dg.AssetExecutionContext) -> None:
+    context.log.info("Ingesting location data into dimension table...")
+    ingest_locations()
+
+
+@dg.asset(deps=[dim_location])
 def bronze_weather(context: dg.AssetExecutionContext) -> None:
     context.log.info("Ingesting weather data into bronze layer...")
     write_bronze_weather()
--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,9 +1,17 @@
 from dagster import Definitions
 
 from orchestration.assets.weather import (
+    dim_location,
     bronze_weather,
     fact_weather,
     silver_weather_hourly,
 )
 
-defs = Definitions(assets=[bronze_weather, silver_weather_hourly, fact_weather])
+defs = Definitions(
+    assets=[
+        dim_location,
+        bronze_weather,
+        silver_weather_hourly,
+        fact_weather,
+    ]
+)
